56919a59659210edcda15e7df803d6d38fea4573
Diuliendy <141693330+Diuliendy@users.noreply.github.com>
2025-04-15 23:02:16 -0300

    Add files via upload

---
+++ questao02/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+import java.util.Scanner;
+public class Principal
+{
+    public static void main(String[]args){
+        Scanner s =new Scanner(System.in);
+        System.out.println("Digite um número");
+        Float N1=s.nextFloat();
+        System.out.println("Digite um número");
+        Float N2= s.nextFloat();
+        Float mult=(N1*N2);
+        System.out.println(mult);
+    }
+    
+}  
