8ae934fd972446a0d272a5d97aab55bc260ca376
Diuliendy <141693330+Diuliendy@users.noreply.github.com>
2025-05-05 07:44:37 -0600

    Add files via upload

---
+++ src/br/edu/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite o peso da pessoa em quilos: ");
+		double pesoEmQuilos = scanner.nextDouble();
+
+		double pesoEmGramas = pesoEmQuilos * 1000;
+
+		System.out.printf("O peso da pessoa em gramas é: %.2f g\n", pesoEmGramas);
+
+		scanner.close();
+	}
+}
